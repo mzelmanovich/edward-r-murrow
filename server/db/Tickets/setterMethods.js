628373e4388dda4a42485fa48e3e7256f4a43299
Mitchell Zelmanovich <mzelmanovich@gmail.com>
2017-04-20 16:20:22 -0400

    removed debug console.log

--- server/db/Tickets/setterMethods.js
+++ server/db/Tickets/setterMethods.js
@@ -1,6 +1,5 @@
 const via = function({source}){
   if (source && source.rel === 'follow_up'){
-    console.log(source.from.ticket_id);
     this.setDataValue('follow_up_id', source.from.ticket_id);
   }
 };
