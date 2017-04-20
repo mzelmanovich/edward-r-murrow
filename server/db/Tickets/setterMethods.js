bd0d3bcc7b33dce536d8f2c675d451e43aff397e
Mitchell Zelmanovich <mzelmanovich@gmail.com>
2017-04-20 14:58:15 -0400

    added console log to see what id is.

--- server/db/Tickets/setterMethods.js
+++ server/db/Tickets/setterMethods.js
@@ -1,5 +1,6 @@
 const via = function({source}){
   if (source && source.rel === 'follow_up'){
+    console.log(source.from.ticket_id);
     this.setDataValue('follow_up_id', source.from.ticket_id);
   }
 };
